296d718ac159d93a1124d595bdc06b85d7262486
tks10 <tktktks10@gmail.com>
2019-08-31 03:05:22 +0900

    feat: app bar

--- app/src/main/java/jp/co/musclecamp/MainActivity.kt
+++ app/src/main/java/jp/co/musclecamp/MainActivity.kt
@@ -1,12 +1,22 @@
 package jp.co.musclecamp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setUpAppBar()
+    }
+
+    private fun setUpAppBar() {
+        val navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolBar.setupWithNavController(navController, appBarConfiguration)
     }
 }
