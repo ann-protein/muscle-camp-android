ec3b8b046468bf233c33c06e27dfa9bfd17eb822
tks10 <tktktks10@gmail.com>
2019-08-31 02:42:57 +0900

    feat: styles

--- app/src/main/java/jp/co/musclecamp/view/muscleposts/MusclePostsFragment.kt
+++ app/src/main/java/jp/co/musclecamp/view/muscleposts/MusclePostsFragment.kt
@@ -1,5 +1,6 @@
 package jp.co.musclecamp.view.muscleposts
 
 import androidx.fragment.app.Fragment
+import jp.co.musclecamp.R
 
-class MusclePostsFragment : Fragment()
+class MusclePostsFragment : Fragment(R.layout.fragment_muscle_posts)
