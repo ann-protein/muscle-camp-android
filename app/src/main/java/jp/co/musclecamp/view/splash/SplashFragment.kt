350a9a6d3d557f90491f490cc63cca67363a374d
tks10 <tktktks10@gmail.com>
2019-11-01 00:24:11 +0900

    feat: token api

--- app/src/main/java/jp/co/musclecamp/view/splash/SplashFragment.kt
+++ app/src/main/java/jp/co/musclecamp/view/splash/SplashFragment.kt
@@ -1,7 +1,12 @@
 package jp.co.musclecamp.view.splash
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import jp.co.musclecamp.R
+import jp.co.musclecamp.data.Repository
+import jp.co.musclecamp.model.TokenManager
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -10,4 +15,22 @@ import kotlin.coroutines.CoroutineContext
 class SplashFragment : Fragment(R.layout.fragment_splash), CoroutineScope {
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main + Job()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Repository.getToken()?.let {
+            signIn(it)
+        } ?: goToSignUp()
+    }
+
+    private fun signIn(token: String) {
+        TokenManager.put(token)
+        findNavController().navigate(R.id.action_splash_to_muscle_posts)
+    }
+
+
+    private fun goToSignUp() {
+
+    }
 }
