6d23ebd92b0987c0a0b393366683ddf6fc372d8d
Haraboo <harada-kyohei-fj@ynu.jp>
2019-11-11 17:12:30 +0900

    Add MusclePostsFragment

--- app/src/main/java/jp/co/musclecamp/view/muscleposts/MusclePostsFragment.kt
+++ app/src/main/java/jp/co/musclecamp/view/muscleposts/MusclePostsFragment.kt
@@ -1,6 +1,25 @@
 package jp.co.musclecamp.view.muscleposts
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import jp.co.musclecamp.R
+import jp.co.musclecamp.data.Repository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class MusclePostsFragment : Fragment(R.layout.fragment_muscle_posts)
+class MusclePostsFragment : Fragment(R.layout.fragment_muscle_posts), CoroutineScope {
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + Job()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        launch {
+            Repository.getMusclePosts()
+        }
+    }
+}
