f030943cadc54f557f3ebaf2ad6d943d9003b523
tks10 <tktktks10@gmail.com>
2019-12-09 15:21:04 +0900

    feat: dark theme

--- app/src/main/java/jp/co/musclecamp/MainActivity.kt
+++ app/src/main/java/jp/co/musclecamp/MainActivity.kt
@@ -2,10 +2,13 @@ package jp.co.musclecamp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES)
     }
 }
